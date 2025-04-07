051546e892bb8fd3c9670c3ca70c11db53448720
ayushrawat98 <aayushrawat0@gmail.com>
2025-04-07 21:38:13 +0530

    Added helmet

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express"
 import { sync } from "./models/sync.js"
 import { router as PostRouter } from './routes/post.route.js'
 import { router as AuthRouter } from './routes/auth.route.js'
+// import { router as UserRouter } from './routes/user.route.js'
 import cors from 'cors'
 import compression from "compression"
 import path from 'path'
-import {fileURLToPath} from 'url'
+import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 import https from 'https'
 import fs from 'fs'
@@ -13,18 +14,26 @@ import fs from 'fs'
 const app = express()
 dotenv.config()
 sync()
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors())
+}
 
 app.use(compression())
-if (process.env.NODE_ENV === 'development') {
-app.use(cors())
+
+if (process.env.NODE_ENV === 'production') {
+    //safety
+    app.use(helmet({
+        contentSecurityPolicy: false,
+    }))
 }
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)) , "public/angular/browser"), {cacheControl : true, maxAge : "1h"}));
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)) , "public/files"), {cacheControl : true, maxAge : "1h"}));
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "public/angular/browser"), { cacheControl: true, maxAge: "1h" }));
+app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), "public/files"), { cacheControl: true, maxAge: "1h" }));
 app.use(express.json())
 app.use('/api/auth', AuthRouter)
-app.use('/api', PostRouter)
-app.get('*', (req, res, next) =>{
-    return res.sendFile(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public', 'angular','browser', 'index.html'))
+app.use('/api/post', PostRouter)
+// app.use('/api/user', UserRouter)
+app.get('*', (req, res, next) => {
+    return res.sendFile(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public', 'angular', 'browser', 'index.html'))
 })
 
 
