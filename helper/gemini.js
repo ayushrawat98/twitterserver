e33a379c4ec81d00369a0df4fceadb6c7f4a7f38
ayushrawat98 <aayushrawat0@gmail.com>
2025-04-10 11:00:04 +0530

    Gemini update. Removed precision.

--- helper/gemini.js
+++ helper/gemini.js
@@ -9,9 +9,9 @@ export async function getGeminiResponse(text) {
     model: "gemini-2.0-flash",
     contents: text,
     config: {
-        maxOutputTokens: 400,
-        temperature: 0.8,
-        systemInstruction: "Your name is @aloo. You are an AI for website called khichdi.life . Be precise with your answers. Dont write @aloo in your answers.",
+        maxOutputTokens: 390,
+        temperature: 0.7,
+        systemInstruction: "Your name is @aloo. You are an AI for website called khichdi.life . Dont write @aloo in your answers.",
       },
   });
   return response.text
