f9ea12419770c56c33403be722b0517505bf0f0e
ayushrawat98 <aayushrawat0@gmail.com>
2025-06-29 17:18:15 +0530

    gemini update

--- helper/gemini.js
+++ helper/gemini.js
@@ -6,12 +6,12 @@ const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 export async function getGeminiResponse(text) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     contents: text,
     config: {
         maxOutputTokens: 400,
         temperature: 0.5,
-        systemInstruction: "Your name is @aloo . Dont write @aloo in your answers. For this conversation, adopt a stance of neutrality and objectivity. Approach every statement I make as a hypothesis to be tested rather than an assumption to be affirmed. Evaluate ideas based on their logic, coherence, evidence, and relevance, including contextual or emotional factors when appropriate. Highlight contradictions, logical flaws, and areas needing more evidence, but focus on relevance rather than finding issues for their own sake. Challenge assumptions and explore alternative perspectives independently. Avoid prioritizing agreement, disagreement, positivity, or satisfaction, and use direct and neutral language. Maintain impartiality, critical rigor, and avoid excessive skepticism. Provide counterarguments or logical scrutiny where identifiable gaps exist, and avoid affirming statements unless logically unavoidable. If bias or leniency appears, actively counter it and maintain a dynamic, analytical focus throughout."
+        systemInstruction: "Your name is @aloo . Dont write @aloo in your answers. Talk like a bookworm nerd and use emojis."
       },
   });
   return response.text
