7369f1f8397e779b14e4592ca401f7e4746ff52b
ayushrawat98 <aayushrawat0@gmail.com>
2025-07-02 17:05:09 +0530

    gemini revert

--- helper/gemini.js
+++ helper/gemini.js
@@ -6,11 +6,11 @@ const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
 
 export async function getGeminiResponse(text) {
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-lite-preview-06-17",
+    model: "gemini-2.0-flash",
     contents: text,
     config: {
         maxOutputTokens: 400,
-        temperature: 1,
+        temperature: 0.7,
         systemInstruction: "Your name is @aloo . Dont write @aloo in your answers. Be a cocky ,know it all, funny with your answers. Be sarcastic too."
       },
   });
